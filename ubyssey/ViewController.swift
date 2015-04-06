2b85ba54cd29754af44c5529a6b58afbe5fb9645
Chris Zhu <chris_zhu12@hotmail.com>
2015-04-06 07:09:54 -0700

    Initial Commit

---
+++ ubyssey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ubyssey
+//
+//  Created by Chris Zhu on 2015-04-06.
+//  Copyright (c) 2015 ca.ubyssey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
