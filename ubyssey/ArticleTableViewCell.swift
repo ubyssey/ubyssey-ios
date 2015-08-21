0e3364435e123e19f25ccd50f5eaf973c3d06194
Peter Siemens <peterjsiemens@gmail.com>
2015-08-21 13:21:32 -0600

    add LFT Etica fonts

--- ubyssey/ArticleTableViewCell.swift
+++ ubyssey/ArticleTableViewCell.swift
@@ -18,8 +18,16 @@ class ArticleTableViewCell: UITableViewCell {
     @IBOutlet weak var articleSection: UILabel!
     
     func setData(article: Article) {
+        
         articleSection.text = article.section.uppercaseString
+        
+        articleTimeSince.font = UIFont(name: "LFTEtica-SemiBold", size: 10)
+        articleSection.font = UIFont(name: "LFTEtica-SemiBold", size: 10)
+
         articleTitle.text = article.longHeadline
+        articleTitle.font = UIFont(name: "LFTEtica-Book", size: 18)
+        
+        
         articleImageView.sd_setImageWithURL(NSURL(string: article.imageUrl))
         articleImageView.clipsToBounds = true
     }
