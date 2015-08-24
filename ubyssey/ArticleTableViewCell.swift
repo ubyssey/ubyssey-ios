56a64ae282c3cc62951379d64a6c0f9404bbbd70
Nathan Yee <ny.nathan.yee@gmail.com>
2015-08-24 11:32:49 -0700

    Implement ArticleTableViewCell.articlePublishedAt

--- ubyssey/ArticleTableViewCell.swift
+++ ubyssey/ArticleTableViewCell.swift
@@ -30,5 +30,75 @@ class ArticleTableViewCell: UITableViewCell {
         
         articleImageView.sd_setImageWithURL(NSURL(string: article.imageUrl))
         articleImageView.clipsToBounds = true
+
+        // If article was published between the current time and midnight of
+        // the current day, show "HH:mm a", else show "yyyy-MM-dd"
+        if article.publishedAt.0 != nil && article.publishedAt.1 != nil {
+            let currentDate = NSDate()
+            let currentDateFormatter = NSDateFormatter()
+            currentDateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+            if getyyyyMMdd((currentDate, currentDateFormatter)) ==
+                getyyyyMMdd((article.publishedAt.0!, article.publishedAt.1!)) {
+                articlePublishedAt.text = getHHmm_a((article.publishedAt.0!, article.publishedAt.1!))
+                if articlePublishedAt.text == nil {
+                    articlePublishedAt.text = ""
+                }
+            } else {
+                let testStr = article.publishedAt.1!.stringFromDate(article.publishedAt.0!)
+                articlePublishedAt.text = getyyyyMMdd((article.publishedAt.0!, article.publishedAt.1!))
+                if articlePublishedAt.text == nil {
+                    articlePublishedAt.text = ""
+                }
+            }
+            if articlePublishedAt.text == nil {
+                articlePublishedAt.text = ""
+            }
+        } else {
+            articlePublishedAt.text = ""
+        }
+        // TODO implement
+        articleTimeSince.text = ""
+    }
+
+    private func getyyyyMMdd(publishedAt: (NSDate, NSDateFormatter))
+            -> String? {
+        if publishedAt.1.dateFormat == "yyyy-MM-dd'T'HH:mm:ssZ" {
+            var dateString = publishedAt.1.stringFromDate(publishedAt.0)
+            dateString.removeRange(advance(dateString.startIndex, 10)..<dateString.endIndex)
+            return dateString
+        }
+        return nil
+    }
+
+    private func getHHmm_a(publishedAt: (NSDate, NSDateFormatter)) -> String? {
+        if publishedAt.1.dateFormat == "yyyy-MM-dd'T'HH:mm:ssZ" {
+            var dateString = publishedAt.1.stringFromDate(publishedAt.0)
+            // e.g. "2001-01-01T01:02:03-0000" -> "01:01:01-0000"
+            dateString.removeRange(advance(dateString.startIndex, 0)..<advance(dateString.startIndex, 11))
+            // e.g. "01:01:01-0000" -> "01:02"
+            dateString.removeRange(advance(dateString.startIndex, 5)..<advance(dateString.endIndex, 0))
+
+            // e.g. "01:02" -> "1:02 AM"
+            // e.g. "12:01" -> "12:01 PM"
+            var dateHour = dateString
+            dateHour.removeRange(advance(dateString.startIndex, 2)..<advance(dateString.endIndex, 0))
+            if let hour = dateHour.toInt() {
+                if hour > 12 {
+                    // truncate leading hours
+                    dateString.removeRange(advance(dateString.startIndex, 0)..<advance(dateString.startIndex, 2))
+                    return String(hour - 12) + dateString + " PM"
+                }
+                else if hour == 12 {
+                    return dateString + " PM"
+                } else if hour >= 10 {
+                    return dateString + " AM"
+                } else {
+                    // truncate leading 0
+                    dateString.removeRange(advance(dateString.startIndex, 0)..<advance(dateString.startIndex, 1))
+                    return dateString + " AM"
+                }
+            }
+        }
+        return nil
     }
 }
